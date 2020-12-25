d536a54481ff99939c69caa02e2f80f1acd40780
singha55162326 <63039299+singha55162326@users.noreply.github.com>
2020-12-25 11:22:50 +0700

    Update CardBasic.vue

--- src/views/ui-elements/card/CardBasic.vue
+++ src/views/ui-elements/card/CardBasic.vue
@@ -1,94 +1,134 @@
-<<template>
-  <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="#28E5B9"
-      dark
-      shrink-on-scroll
-      prominent
-      src="https://picsum.photos/1920/1080?random"
-      fade-img-on-scroll
-      scroll-target="#scrolling-techniques-3"
-    >
-      <template v-slot:img="{ props }">
-       
-      </template>
-
-
-      <v-app-bar-title>Project</v-app-bar-title>
-
-      <v-spacer></v-spacer>
-
-    
-
-      <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Project</v-tab>
-          <v-tab>History</v-tab>
+<template>
+    <v-card>
+        <v-tabs
+                v-model="tab"
+                background-color="deep-purple accent-4"
+                centered
+                color="#28E5B9"
+              
+        >
           
-        </v-tabs>
-        
-      </template>
-      
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-3"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1000px;"></v-container>
-    </v-sheet>
-    
-  </v-card>
- 
+
+            <v-tab href="#tab1">
+                New Project
+            </v-tab>
+
+            <v-tab href="#tab2">
+                    History
+            </v-tab>
+        </v-tabs>    
+        <v-tabs-items v-model="tab">
+            <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    :items-per-page="5"
+                    class="elevation-1"
+            >
+            </v-data-table>
+        </v-tabs-items>
+    </v-card>
 </template>
+
 <script>
   export default {
     data () {
       return {
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
         desserts: [
           {
             name: 'Frozen Yogurt',
             calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
           },
           {
             name: 'Ice cream sandwich',
             calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
           },
           {
             name: 'Eclair',
             calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
           },
           {
             name: 'Cupcake',
             calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
           },
           {
             name: 'Gingerbread',
             calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
           },
           {
             name: 'Jelly bean',
             calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
           },
           {
             name: 'Lollipop',
             calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
           },
           {
             name: 'Honeycomb',
             calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
           },
           {
             name: 'Donut',
             calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
           },
           {
             name: 'KitKat',
             calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
           },
         ],
       }
     },
   }
-</script>  
+</script>
