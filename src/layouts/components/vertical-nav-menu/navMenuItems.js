062ea8a7f8c10e6bf881c1cde909144c780557c9
Your Name <followthe30@gmail.com>
2020-12-25 10:09:12 +0700

    First Commit

---
+++ src/layouts/components/vertical-nav-menu/navMenuItems.js
@@ -0,0 +1,505 @@
+/*=========================================================================================
+  File Name: sidebarItems.js
+  Description: Sidebar Items list. Add / Remove menu items from here.
+  Strucutre:
+          url     => router path
+          name    => name to display in sidebar
+          slug    => router path name
+          icon    => Feather Icon component/icon name
+          tag     => text to display on badge
+          tagColor  => class to apply on badge element
+          i18n    => Internationalization
+          submenu   => submenu of current item (current item will become dropdown )
+                NOTE: Submenu don't have any icon(you can add icon if u want to display)
+          isDisabled  => disable sidebar item/group
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+
+export default [
+  // {
+  //   url: "/apps/email",
+  //   name: "Email",
+  //   slug: "email",
+  //   icon: "MailIcon",
+  //   i18n: "Email",
+  // },
+  
+  // {
+  //   url: null,
+  //   name: 'Dashboard',
+  //   tag: '2',
+  //   tagColor: 'warning',
+  //   icon: 'HomeIcon',
+  //   i18n: 'Dashboard',
+  //   submenu: [
+  //     {
+  //       url: '/dashboard/analytics',
+  //       name: 'Analytics',
+  //       slug: 'dashboard-analytics',
+  //       i18n: 'Analytics'
+  //     },
+  //     {
+  //       url: '/dashboard/ecommerce',
+  //       name: 'eCommerce',
+  //       slug: 'dashboard-ecommerce',
+  //       i18n: 'eCommerce'
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   header: 'Apps',
+  //   icon: 'PackageIcon',
+  //   i18n: 'DASHBOARD',
+  //   items: [
+  //     {
+  //       url: '/dashboard/analytics',
+  //       name: 'Home',
+  //       slug: 'Home',
+  //       icon: 'HomeIcon',
+  //       i18n: 'Home'
+  //     },
+  //     {
+  //       url: null,
+  //       name: 'eCommerce',
+  //       icon: 'ShoppingCartIcon',
+  //       i18n: 'Products',
+  //       submenu: [
+  //         {
+  //           url: '/apps/eCommerce/shop',
+  //           name: 'Shop',
+  //           slug: 'ecommerce-shop',
+  //           i18n: 'Shop'
+  //         },
+  //         {
+  //           url: '/apps/eCommerce/item/',
+  //           name: 'Item Details',
+  //           slug: 'ecommerce-item-detail-view',
+  //           i18n: 'ItemDetails'
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       url: '/apps/todo',
+  //       name: 'Todo',
+  //       slug: 'todo',
+  //       icon: 'CheckSquareIcon',
+  //       i18n: 'Order'
+  //     },
+  //     {
+  //       url: '/apps/calendar/vue-simple-calendar',
+  //       name: 'Booking',
+  //       slug: 'calendar-simple-calendar',
+  //       icon: 'CalendarIcon',
+  //       tagColor: 'success',
+  //       i18n: 'Booking'
+  //     },
+  //     {
+  //       url: '/apps/email',
+  //       name: 'Email',
+  //       slug: 'email',
+  //       icon: 'LogOutIcon',
+  //       i18n: 'Logout'
+  //     }
+  //   ]
+  // },
+
+  {
+    header: 'UI',
+    icon: 'LayersIcon',
+    i18n: 'Dashboard',
+    items: [
+
+      {
+        url: null,
+        name: 'Grid',
+        icon: 'LayoutIcon',
+        i18n: 'CFO',
+        submenu: [
+          {
+            url: '/ui-elements/grid/vuesax',
+            name: 'Vuesax',
+            slug: 'grid-vuesax',
+            i18n: 'Dashboard'
+          },
+          {
+            url: '/ui-elements/grid/tailwind',
+            name: 'Tailwind',
+            slug: 'grid-tailwind',
+            i18n: 'CFO Approvement'
+          }
+        ]
+      },
+      {
+        url: null,
+        name: 'Data List',
+        tagColor: 'primary',
+        icon: 'ListIcon',
+        i18n: 'Sale',
+        submenu: [
+          {
+            url: '/ui-elements/card/basic',
+            name: 'Projects',
+            slug: 'basic-cards',
+            i18n: 'Projects'
+          },
+          {
+             url: '/components/chip',
+             name: 'Chip',
+             slug: 'component-chip',
+             i18n: 'Sale/Marketing manager'
+           },
+          {
+            url:'/ui-elements/colors',
+            name:'minute of meeting',
+            slug:'colors',
+            icon:'DropletIcon',
+            i18n:'customer List'
+          },
+          {
+            url: '/apps/todo',
+            name: 'Todo',
+            slug: 'todo',
+            icon: 'CheckSquareIcon',
+            i18n: 'Customer Requirement'
+          },
+          
+          {
+            url: '/ui-elements/card/statistics',
+            name: 'Statistics',
+            slug: 'statistics-cards',
+            i18n: 'Customer contract'
+          },
+        
+          {
+            url: '/ui-elements/card/card-actions',
+            name: 'Card Actions',
+            slug: 'card-actions',
+            i18n: 'Quotation'
+          },
+          {
+            url: '/ui-elements/card/card-colors',
+            name: 'Card Colors',
+            slug: 'card-colors',
+            i18n: 'Invoice'
+          },
+         
+          {
+               url: '/components/dialogs',
+             name: 'Dialogs',
+             slug: 'component-dialog',
+             i18n: 'Payment'
+            
+         },
+         {
+             url: '/components/divider',
+             name: 'Divider',
+             slug: 'component-divider',
+             i18n: 'Receipt'
+           },
+        ]
+      },
+      // {
+      //   url: '/ui-elements/colors',
+      //   name: 'Colors',
+      //   slug: 'colors',
+      //   icon: 'DropletIcon',
+      //   i18n: 'Colors'
+      // },
+      {
+        url: null,
+        name: 'Card',
+        icon: 'CreditCardIcon',
+        i18n: 'Finance',
+        submenu: [
+            {
+            url: '/components/loading',
+            name: 'Loading',
+            slug: 'component-loading',
+           i18n: 'Project'
+           },
+          {
+            url: '/extensions/clipboard',
+            name: 'Clipboard',
+            icon: 'CopyIcon',
+            slug: 'extra-component-clipboard',
+            i18n: 'Quotation'
+          },
+          {
+            url: '/extensions/context-menu',
+            name: 'Context Menu',
+            icon: 'MoreHorizontalIcon',
+            slug: 'extra-component-context-menu',
+            i18n: 'Invoice'
+          },
+          {
+            url: '/extensions/star-ratings',
+            name: 'Star Ratings',
+            icon: 'StarIcon',
+            slug: 'extra-component-star-ratings',
+            i18n: 'Payment'
+          },
+          {
+            url: '/extensions/autocomplete',
+            name: 'Autocomplete',
+            icon: 'Edit3Icon',
+            slug: 'extra-component-autocomplete',
+            i18n: 'Receipt'
+          },
+          {
+            url: '/extensions/tree',
+            name: 'Tree',
+            icon: 'GitPullRequestIcon',
+            slug: 'extra-component-tree',
+            i18n: 'Request payment'
+          }
+        ]
+      },
+      {
+        url: null,
+        name: 'Components',
+        icon: 'ArchiveIcon',
+        i18n: 'Procurement',
+        submenu: [
+             {
+             url: '/components/popup',
+             name: 'Popup',
+            slug: 'component-popup',
+            i18n: 'Project'
+           },
+          {
+            url: '/components/tabs',
+            name: 'Tabs',
+            slug: 'component-tabs',
+            i18n: 'Supplier selection'
+          },
+          {
+            url: '/components/alert',
+            name: 'Alert',
+            slug: 'component-alert',
+            i18n: 'Request Payment'
+          },
+          {
+            url: '/components/avatar',
+            name: 'Avatar',
+            slug: 'component-avatar',
+            i18n: 'Supplier Contract'
+          },
+          {
+            url: '/components/breadcrumb',
+            name: 'Breadcrumb',
+            slug: 'component-breadcrumb',
+            i18n: 'Purchase Order'
+          },
+          {
+            url: '/components/button',
+            name: 'Button',
+            slug: 'component-button',
+            i18n: 'Product'
+          },
+          {
+            url: '/components/button-group',
+            name: 'Button Group',
+            slug: 'component-button-group',
+            i18n: 'Delivery Products'
+          }
+          // {
+          //   url: '/components/chip',
+          //   name: 'Chip',
+          //   slug: 'component-chip',
+          //   i18n: 'Chip'
+          // },
+          // {
+          //   url: '/components/collapse',
+          //   name: 'Collapse',
+          //   slug: 'component-collapse',
+          //   i18n: 'Collapse'
+          // },
+          // {
+          //   url: '/components/dialogs',
+          //   name: 'Dialogs',
+          //   slug: 'component-dialog',
+          //   i18n: 'Dialogs'
+          // },
+          // {
+          //   url: '/components/divider',
+          //   name: 'Divider',
+          //   slug: 'component-divider',
+          //   i18n: 'Divider'
+          // },
+          // {
+          //   url: '/components/dropdown',
+          //   name: 'DropDown',
+          //   slug: 'component-drop-down',
+          //   i18n: 'DropDown'
+          // },
+          // {
+          //   url: '/components/list',
+          //   name: 'List',
+          //   slug: 'component-list',
+          //   i18n: 'List'
+          // },
+          // {
+          //   url: '/components/loading',
+          //   name: 'Loading',
+          //   slug: 'component-loading',
+          //   i18n: 'Loading'
+          // },
+          // {
+          //   url: '/components/navbar',
+          //   name: 'Navbar',
+          //   slug: 'component-navbar',
+          //   i18n: 'Navbar'
+          // },
+          // {
+          //   url: '/components/notifications',
+          //   name: 'Notifications',
+          //   slug: 'component-notifications',
+          //   i18n: 'Notifications'
+          // },
+          // {
+          //   url: '/components/pagination',
+          //   name: 'Pagination',
+          //   slug: 'component-pagination',
+          //   i18n: 'Pagination'
+          // },
+          // {
+          //   url: '/components/popup',
+          //   name: 'Popup',
+          //   slug: 'component-popup',
+          //   i18n: 'Popup'
+          // },
+          // {
+          //   url: '/components/progress',
+          //   name: 'Progress',
+          //   slug: 'component-progress',
+          //   i18n: 'Progress'
+          // },
+          // {
+          //   url: '/components/sidebar',
+          //   name: 'Sidebar',
+          //   slug: 'component-sidebar',
+          //   i18n: 'Sidebar'
+          // },
+          // {
+          //   url: '/components/slider',
+          //   name: 'Slider',
+          //   slug: 'component-slider',
+          //   i18n: 'Slider'
+          // },
+
+          // {
+          //   url: '/components/tooltip',
+          //   name: 'Tooltip',
+          //   slug: 'component-tooltip',
+          //   i18n: 'Tooltip'
+          // },
+          // {
+          //   url: '/components/upload',
+          //   name: 'Upload',
+          //   slug: 'component-upload',
+          //   i18n: 'Upload'
+          // }
+        ]
+      },
+      {
+        url: null,
+        name: 'Extensions',
+        icon: 'PlusCircleIcon',
+        i18n: 'Design',
+        submenu: [
+          {
+            url: '/extensions/select',
+            name: 'Select',
+            icon: 'PocketIcon',
+            slug: 'extra-component-select',
+            i18n: 'Upload Artwork'
+          }
+          // {
+          //   url: '/extensions/quill-editor',
+          //   name: 'Quill Editor',
+          //   icon: 'EditIcon',
+          //   slug: 'extra-component-quill-editor',
+          //   i18n: 'QuillEditor'
+          // },
+          // {
+          //   url: '/extensions/drag-and-drop',
+          //   name: 'Drag & Drop',
+          //   icon: 'DropletIcon',
+          //   slug: 'extra-component-drag-and-drop',
+          //   i18n: 'DragAndDrop'
+          // },
+          // {
+          //   url: '/extensions/datepicker',
+          //   name: 'Datepicker',
+          //   icon: 'CalendarIcon',
+          //   slug: 'extra-component-datepicker',
+          //   i18n: 'Datepicker'
+          // },
+          // {
+          //   url: '/extensions/datetime-picker',
+          //   name: 'Datetime Picker',
+          //   icon: 'ClockIcon',
+          //   slug: 'extra-component-datetime-picker',
+          //   i18n: 'DatetimePicker'
+          // },
+          // {
+          //   url: '/extensions/access-control',
+          //   name: 'Access Control',
+          //   slug: 'extra-component-access-control',
+          //   i18n: 'AccessControl'
+          // },
+          // {
+          //   url: '/extensions/i18n',
+          //   name: 'I18n',
+          //   slug: 'extra-component-i18n',
+          //   i18n: 'I18n'
+          // },
+          // {
+          //   url: '/extensions/carousel',
+          //   name: 'Carousel',
+          //   icon: 'LayersIcon',
+          //   slug: 'extra-component-carousel',
+          //   i18n: 'Carousel'
+          // },
+          
+          // // {
+          // //   name: "Import/Export",
+          // //   i18n: "Import/Export",
+          // //   submenu: [
+
+          // //   ]
+          // // },
+          // {
+          //   url: '/import-export/import',
+          //   name: 'Import',
+          //   icon: 'HomeIcon',
+          //   slug: 'import',
+          //   i18n: 'Import'
+          // },
+          // {
+          //   url: '/import-export/export',
+          //   name: 'Export',
+          //   icon: 'HomeIcon',
+          //   slug: 'export',
+          //   i18n: 'Export'
+          // },
+          // {
+          //   url: '/import-export/export-selected',
+          //   name: 'Export Selected',
+          //   icon: 'HomeIcon',
+          //   slug: 'export-selected',
+          //   i18n: 'ExportSelected'
+          // }
+        ]
+      }
+    ]
+  },
+  
+  
+]
+
---
+++ src/views/ui-elements/card/CardBasic.vue
@@ -0,0 +1,94 @@
+<<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="#28E5B9"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+      <template v-slot:img="{ props }">
+       
+      </template>
+
+
+      <v-app-bar-title>Project</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+    
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Project</v-tab>
+          <v-tab>History</v-tab>
+          
+        </v-tabs>
+        
+      </template>
+      
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-3"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+    
+  </v-card>
+ 
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+      }
+    },
+  }
+</script>  
